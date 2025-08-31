64a3a5c72474de90c366433d97018f77899a5929
frrst-ian <frrstxd@proton.me>
2025-09-01 06:19:52 +0800

    feat: add LogIn component: handles login auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,83 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
+
+const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+    setError("");
+
+    fetch("http:localhost:3000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then(async (response) => {
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(
+            data.message || `Error ${response.status}: Login Failed`
+          );
+        }
+
+        localStorage.setItem("token", data.token);
+        navigate("/admin/posts");
+
+        setSubmitting(false);
+
+        return data;
+      })
+      .catch((err) => {
+        setError(err.message);
+        setSubmitting(false);
+      });
+  };
+
+  return (
+    <div className="login">
+      {error && <div> {error}</div>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit" disabled={submitting}>
+          {submitting ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div className="app">
-      <h1>Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth/login" element={<LogIn />} />
+        <Route path="/admin/posts" element={<PostList />} />
+      </Routes>
+    </BrowserRouter>
   );
-}; 
+};
 export default App;
