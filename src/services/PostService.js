3f22a03dd8695f64815d5432d145d84ee4692e18
frrst-ian <frrstxd@proton.me>
2025-09-07 16:44:13 +0800

    chore: change base url to new backend url

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://forrest-blog-backend.onrender.com";
 
 export async function auth(email, password) {
     const res = await fetch(`${BASE_URL}/auth/login`, {
